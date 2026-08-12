46ead38d75c7b52e13c7aa6c1c3b08a82be1c67b
unknown <suryaprk165@gmail.com>
2026-08-12 13:39:01 +0530

    implemented google Authentication

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,10 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Tell Next.js to treat the client folder as the tracing root
+  outputFileTracingRoot: __dirname,
   images: {
     remotePatterns: [
       {
